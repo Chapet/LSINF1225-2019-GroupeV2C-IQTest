61c3b53a3a2052d2987bc465baffc404a8d56034
Marques <mathias.marques@student.uclouvain.be>
2019-04-29 14:48:23 +0200

    chgmt chanson

--- iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/MainActivity.java
+++ iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ring= MediaPlayer.create(MainActivity.this,R.raw.cigale);
+        ring.prepareAsync();
         ring.start();
+        ring.setLooping(true);
         this.tapbut = (TextView) findViewById(R.id.tap);
         tapbut.setOnClickListener(new View.OnClickListener() {
             @Override
