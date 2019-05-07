64895aa0931c7a54f47cb3c200279089ad491525
Justine <justine.pironnet@student.uclouvain.be>
2019-05-07 17:45:12 +0200

    Add addfriend + see my mistakes

--- iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/menu.java
+++ iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/menu.java
@@ -1,10 +1,12 @@
 package be.uclouvain.lsinf1225.groupeV2C.iqtest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,12 +23,15 @@ public class menu extends AppCompatActivity {
     private TextView completetest;
     private TextView quicktest;
     private TextView flashtest;
+    private menu addfriends;
     DrawerLayout dLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        this.addfriends=this;
         this.categories = (TextView) findViewById(R.id.categories);
         categories.setOnClickListener(new View.OnClickListener() {
             @Override
--- iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/see_my_mistakes.java
+++ iqwhizz/app/src/main/java/be/uclouvain/lsinf1225/groupeV2C/iqtest/see_my_mistakes.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-public class addfriend extends AppCompatActivity {
+public class see_my_mistakes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addfriend);
+        setContentView(R.layout.activity_see_my_mistakes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // get the reference of Toolbar
         setSupportActionBar(toolbar); // Setting/replace toolbar as the ActionBar
         toolbar.setTitle(getResources().getString(R.string.Add_a_friend));
